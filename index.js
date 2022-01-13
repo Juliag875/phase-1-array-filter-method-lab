23423ebdedc94fb71521e40c2c91baee56d53f11
Iuliia El Sammak <juliag875@gmail.com>
2022-01-13 18:56:00 -0500

    commit tree d08adf2a72a4e9ced53d76ee48d02344af6f88fa

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return matchingDrivers;
+}
+
+function fuzzyMatch (drivers, query) {
+    const fuzzy = drivers.filter(driver => driver[0] === query[0]);
+    return fuzzy;
+}
+
+function matchName(drivers, str) {
+    const match = drivers.filter(driver => driver.name === str );
+    return match;
+}
